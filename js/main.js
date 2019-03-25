26c4936cb63648ce6483ac448303abf4bebb1d08
mwu233 <40613916+mwu233@users.noreply.github.com>
2019-03-24 21:45:38 -0500

    Initialized

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Javascripts by Meiliu Wu, 2019
+
+/*eslint-env jquery*/
+/*eslint-disable no-extra-semi*/
+/*eslint-disable no-unused-vars*/
+/*eslint-disable no-undef*/
+/*eslint-disable no-console*/
+
+/* Map of GeoJSON data from Pop.geojson */
